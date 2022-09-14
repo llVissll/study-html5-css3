a334d7f791ecd8d3a1bfd0dd40147753854bb07c
João Victor Felix <joao.victor.akali@outlook.com>
2022-09-14 17:36:53 -0300

    Novo projeto

---
+++ Facul_Macei/script.js
@@ -0,0 +1,14 @@
+function calcular(){
+    let kml1 = document.getElementById('kmlitro')
+    let kmp1 = document.getElementById('kmper')
+    let kml = Number(kml1.value)
+    let kmp = Number(kmp1.value)
+    let res = document.getElementById('res')
+    let custo1 = document.getElementById('custo') 
+    let custo = Number(custo1.value)
+    
+
+    let calc = kmp / kml
+    let result = calc * custo
+    res.innerHTML = `Você irá gastar um total de R$${result.toFixed(2)} `
+}
