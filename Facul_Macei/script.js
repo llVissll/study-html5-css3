d1931070e00377775305caa08ffff6c86083fd77
João Victor Felix <joao.victor.akali@outlook.com>
2022-09-15 23:41:45 -0300

    Update script.js

--- Facul_Macei/script.js
+++ Facul_Macei/script.js
@@ -6,9 +6,21 @@ function calcular(){
     let res = document.getElementById('res')
     let custo1 = document.getElementById('custo') 
     let custo = Number(custo1.value)
-    
-
     let calc = kmp / kml
     let result = calc * custo
-    res.innerHTML = `Você irá gastar um total de R$${result.toFixed(2)} `
+    
+
+    if(custo == 0 ){
+        alert('Preencha o preço do combustível em sua cidade.')
+    }
+    else if(kml == 0 ){
+        alert('Preencha quantos Km/L seu veículo consome.')
+    }
+    else if(kmp == 0 ){
+        alert('Preencha quantos Km deseja percorrer.')
+
+    }
+    else{
+        res.innerHTML = `Você irá gastar um total de R$${result.toFixed(2)} com combustível. ` 
+    }
 }
