20d63be54a534fda798a2ceba9e073e6d8445fe1
João Victor Felix <joao.victor.akali@outlook.com>
2023-03-03 22:58:33 -0300

    sorteio de letras

---
+++ sorteio-letras/script.js
@@ -0,0 +1,186 @@
+ 
+function sortear(){
+    let res = document.querySelector('#res')
+    let res1 = document.querySelector('#res1')
+    let numeroSorteado = document.querySelector("#res")
+    numeroSorteado = Math.floor(Math.random() * 26) + 1;
+    res.classList.add("lista")
+    if(numeroSorteado === 1){
+      numeroSorteado = "A"  
+      res.innerHTML = numeroSorteado 
+      res1.innerHTML = "Hummm, Letra A?, com certeza a primeira palavra que me vem na cabeça é <Strong> Amor !" 
+       
+    }else if(numeroSorteado === 2){
+        numeroSorteado = "B"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com B?, me veio <Strong> Brilho </Strong> na cabeça KKKKK, acho q tem tudo a ver com você." 
+         
+      }
+      else if(numeroSorteado === 3){
+        numeroSorteado = "C"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com C, me veio <Strong> Coisinha linda </Strong> KKKK, acho que é pq eu falo isso a cada 1 minuto." 
+         
+      }  
+      else if(numeroSorteado === 4){
+        numeroSorteado = "D"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Letra D, sem dúvidas você é uma pessoa muito<Strong> Divertida</Strong> amor!" 
+         
+      }else if(numeroSorteado === 5){
+        numeroSorteado = "E"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com E, com certeza <Strong> Educada! </Strong>" 
+         
+      }else if(numeroSorteado === 6){
+        numeroSorteado = "F"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com F, sem dúvidas você é muito <Strong> Forte! </Strong> amor, saiba disso!" 
+         
+      }else if(numeroSorteado === 7){
+        numeroSorteado = "G"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com G, me veio <Strong> Geniazinha </Strong> KKKKKK, simplesmente pelo fato de saber todos os assuntos!" 
+         
+      }else if(numeroSorteado === 8){
+        numeroSorteado = "H"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = " Com H, pensei em <Strong> Honesta! </Strong> " 
+         
+      }
+      else if(numeroSorteado === 9){
+        numeroSorteado = "I"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com I, acho que não tem outra palavra que definiria melhor do que <Strong> Incrível! </Strong>" 
+         
+      }
+      else if(numeroSorteado === 10){
+        numeroSorteado = "J"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Eita com J?, você <Strong> Joga muita bola </strong> KKKKKK, pra quem me conhece sabe que isso é um grande elogio ta bom?" 
+         
+      }
+      else if(numeroSorteado === 11){
+        numeroSorteado = "K"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Olha, com K, eu realmente não consigo pensar em nenhuma palavra KKKKKK, essa letra só serve pra rir." 
+         
+      }
+      else if(numeroSorteado === 12){
+        numeroSorteado = "L"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Faz o L, Com certeza <Strong> Linda! </strong> <3" 
+         
+      }
+      else if(numeroSorteado === 13){
+        numeroSorteado = "M"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com M, minha nenê é <strong> Maravilhosa! </strong>" 
+         
+      }else if(numeroSorteado === 14){
+        numeroSorteado = "N"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com N, Minha <strong> Nenê! </strong>, e também me vem <strong> Natureza! </strong>." 
+         
+      }
+      else if(numeroSorteado === 15){
+        numeroSorteado = "O"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Letra O, <strong> Oi nenê! </strong> KKKKKK acho que vc até consegue ouvir eu falando isso." 
+         
+      }
+      else if(numeroSorteado === 16){
+        numeroSorteado = "P"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com P, com certeza é <strong> Paciente </strong> KKKKK, pra me falar onde estão as 132 peças do Mega." 
+         
+      }
+      else if(numeroSorteado === 17){
+        numeroSorteado = "Q"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com Q, o que me vem na mente é <strong> Que coisa! </strong> KKKKK, você fala muito isso ta bom? " 
+         
+      }
+      else if(numeroSorteado === 18){
+        numeroSorteado = "R"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com R, eu te acho muito <strong> Responsável </strong> meu amor." 
+         
+      }
+      else if(numeroSorteado === 19){
+        numeroSorteado = "S"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Seu corpo suadoo, KKKKKK isso foi a primeira coisa que me veio na cabeça ta bom? mas você é <strong> Sensacional </strong> nenê!" 
+         
+      }
+      else if(numeroSorteado === 20){
+        numeroSorteado = "T"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com T é fácil, você é muito <strong> Tranquila </strong> amor. Todo mundo fala isso KKKK." 
+         
+      }
+      else if(numeroSorteado === 21){
+        numeroSorteado = "U"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com U , <strong> Uai </strong> KKKKKK. Consigo ouvir você falando isso." 
+         
+      }
+      else if(numeroSorteado === 22){
+        numeroSorteado = "V"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com V, você é muito <strong> Valiosa </strong> amor." 
+         
+      }
+      else if(numeroSorteado === 23){
+        numeroSorteado = "W"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com W não da KKKK, pq foram criar um M ao contrário??" 
+         
+      }
+      else if(numeroSorteado === 24){
+        numeroSorteado = "X"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com X?, você é meu <strong> Xodózin </strong> <3" 
+         
+      }
+      else if(numeroSorteado === 25){
+        numeroSorteado = "Y"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Olha com Y... me veio Yakissoba na cabeça ent é isso KKKKKKKK" 
+         
+      }
+      else if(numeroSorteado === 26){
+        numeroSorteado = "Z"  
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "Com Z é fácil, você é muito <strong> Zelosa </strong> com suas coisas amor, acho isso incrível." 
+         
+      }
+      
+    
+      else{
+        res.innerHTML = numeroSorteado 
+        res1.innerHTML = "é isso" 
+      } 
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+/*function sortear(){
+    let res = document.querySelector('#res')
+    let res1 = document.querySelector('#res1')
+    let numeroSorteado = document.querySelector("#res")
+    numeroSorteado = Math.floor(Math.random() * 100) + 1;
+    res.classList.add("lista")
+    res1.innerHTML = "O número sorteado é:" 
+    res.innerHTML = numeroSorteado
+}*/
+
