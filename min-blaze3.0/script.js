82f3a685f0a74c317f407968bc568ad687039064
João Victor Felix <joao.victor.akali@outlook.com>
2023-03-12 22:50:50 -0300

    att

--- min-blaze3.0/script.js
+++ min-blaze3.0/script.js
@@ -1,9 +1,12 @@
 
 
 function calcular(){
-    let res = document.getElementById("parag")
+    /*let res = document.getElementById("parag")*/
     let n2 = document.getElementById("min")
     let n1 = Number(n2.value)
+    if (n1 === 0){
+        alert('Click no minuto que saiu o branco')
+    }else{    
     if(n1 > 60 || n1 < 0){
         alert(`Digite um número válido`)
     }else{
@@ -38,10 +41,11 @@ function calcular(){
     apagar(valor6)
     marcarS(valor, valor2, valor3, valor4, valor5, valor6)
     marcar(valor, valor2, valor3, valor4, valor5, valor6)
-    
+    dica(valor, valor2, valor3, valor4, valor5, valor6)
+   }
+   document.querySelector(`#min`).value = null
 }
 }
-
 function marcar(a, b, c, d, e, f){
     let div1 = document.getElementById(`n` + a)
     let div2 = document.getElementById(`n` + b)
@@ -56,7 +60,7 @@ function marcar(a, b, c, d, e, f){
         div1.style.backgroundColor = `green`
         div1.style.color = `white`, div1.style.fontWeight = `bolder`
         div1 = document.getElementById(`n` + a).id = `s` + a; 
-    }
+      }
     if(div2 === null){}
     else{
         div2.style.backgroundColor = `green`
@@ -68,7 +72,7 @@ function marcar(a, b, c, d, e, f){
         div3.style.backgroundColor = `green`
         div3.style.color = `white`, div3.style.fontWeight = `bolder`
         div3 = document.getElementById(`n` + c).id = `s` + c;
-    }
+     }
     if(div4 === null){}
     else{
         div4.style.backgroundColor = `green`
@@ -86,17 +90,15 @@ function marcar(a, b, c, d, e, f){
         div6.style.backgroundColor = `green`
         div6.style.color = `white`, div6.style.fontWeight = `bolder`
         div6 = document.getElementById(`n` + f).id = `s` + f;
+
     }  
  
-    
-    
-    
-    
-    
+   
 }
 
 function apagar(a){
-
+    
+      
     for(i = 0; i < 37; i++){
         a++
         if(a > 60){a = 1}
@@ -108,6 +110,8 @@ function apagar(a){
         minuto.style.color = `black`, minuto.style.fontWeight = `100`  
         minuto = document.getElementById(`s` + a).id = `n` + a;   
     }}
+
+ 
 }
 
 
@@ -119,6 +123,16 @@ function negrito(){
         }else{
             minuto.style.backgroundColor = `white`
             minuto.style.color = `black`, minuto.style.fontWeight = `bolder`  
+        }
+    }
+    for(b = 0; b < 120; b++){
+        let casa = document.getElementById(`t` + b)
+        if(casa === null){
+
+        }else{
+            casa.style.backgroundColor = `white`
+            casa.style.color = `black`, casa.style.fontWeight = `100` 
+            casa = document.getElementById(`t` + b).id = `c` + b;   
         }
     }
 }
@@ -166,6 +180,44 @@ function marcarS(a, b, c, d, e, f){
 
 function branco(b){
     document.querySelector(`#min`).value = b
-    
+}
 
+function ticar(min, max){
+ 
+    let f = Math.floor(Math.random() * (max - min) ) + min;
+
+    let div = document.getElementById('c' + f);
+    
+    div.style.color = `white`, div.style.backgroundColor = `blue`;
+     div = document.getElementById('c' + f).id = 't' + f; 
+    
+   
+}
+function dica(a, b, c, d, e, f){
+ 
+        min = a * 2 - 8;
+        max = a * 2 + 2;
+        ticar(min, max)
+
+        min = b * 2 - 5;
+        max = b * 2 + 1;
+        ticar(min, max)
+
+        min = c * 2 - 2;
+        max = c * 2 + 3;
+        ticar(min, max)
+
+        min = d * 2 - 2;
+        max = d * 2 + 4;
+        ticar(min, max)
+    
+        min = e * 2 - 5;
+        max = e * 2 + 1;
+        ticar(min, max)
+   
+        min = f * 2 - 2;
+        max = f * 2 + 4;
+        ticar(min, max)
+  
+   
 }
