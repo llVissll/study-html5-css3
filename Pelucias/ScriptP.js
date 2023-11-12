0b60d9866ef76662e94e92c8f00b75305e3aa6e5
João Victor Felix <joao.victor.akali@outlook.com>
2023-11-11 22:57:05 -0300

    big att in page

--- Pelucias/ScriptP.js
+++ Pelucias/ScriptP.js
@@ -6,3 +6,10 @@ function abrirmenu(){
     
     nav.classList.toggle('active');
 }
+
+
+function enviar(nome, cod, valor, id){
+    var link = `https://api.whatsapp.com/send?phone=5511983014966&text=Olá,%20vim%20pelo%20site%20Maviss%20🐼.%20Gostaria%20de%20saber%20mais%20sobre%20a%20pelúcia:%20` + nome +  '%20-%20Valor:%20' + valor + '%20Código%20' + cod
+    var msg = document.querySelector('#meuLink' + id)
+    msg.href = link
+}
